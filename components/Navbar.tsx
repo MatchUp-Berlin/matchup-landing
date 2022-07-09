b9737ad7fc7857b32380e8368ce6c02354f74771
Maxim Buz <mbuz.maxim@gmail.com>
2022-07-09 13:37:51 +0200

    fixed logo import

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Flex, UnorderedList, ListItem, Button } from '@chakra-ui/react';
 import React from 'react';
 import Image from 'next/image';
-import Logo from '../public/Logo.svg';
+import Logo from '../public/logo.svg';
 import Link from 'next/link';
 
 export interface INavbarProps {}
