e6d5c39c3355a0965349f0da7cb35f2bd291746d
Maxim Buz <mbuz.maxim@gmail.com>
2022-07-13 19:37:56 +0200

    trying to fix color mode shit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import theme from '../theme';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS={true}>
+    <ChakraProvider theme={theme} resetCSS={true}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
